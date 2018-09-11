b34789f1117ed1b883d6b18ea1c7b4b74992edcb
Cu Cong Can <cancc@vitalify.asia>
2018-09-11 13:25:23 +0700

    WebViewApp using sdk v3.0.0

---
+++ app/src/main/java/mbaas/com/nifcloud/androidwebviewapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package mbaas.com.nifcloud.androidwebviewapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var _btn_info: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //ボタンの処理を登録する
+        _btn_info = findViewById<View>(R.id.btnInfo) as Button
+
+        //Informationボタンの処理を実装
+        _btn_info.setOnClickListener {
+            // WebviewActivityに遷移
+            val intent = Intent(applicationContext, WebviewActivity::class.java)
+            startActivityForResult(intent, REQUEST_RESULT)
+        }
+
+
+    }
+
+    companion object {
+
+        private val TAG = "ShopActivity"
+        private val REQUEST_RESULT = 0
+    }
+}
